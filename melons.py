b28b6cc0b868555316d9e9ffa476c4504cf36224
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:55:16 -0700

    Part 2 complete

--- melons.py
+++ melons.py
@@ -15,12 +15,20 @@ class AbstractMelonOrder(object):
 
         self.shipped = True
 
+
+
     def get_total(self):
         """Calculate price."""
 
         base_price = 5
+        
+        if self.species == 'Christmas':
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
         return total
+
+
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -49,3 +57,11 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+
+        if self.qty < 10:
+            return super(InternationalMelonOrder, self).get_total() + 3
+        
+        return super(InternationalMelonOrder, self).get_total()
+
